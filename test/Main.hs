552ac30b095dac95fe6dc6ba56e5e895af5abed8
Scott Kovach <dskovach@gmail.com>
2015-08-20 18:18:16 -0700

    also move old tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,13 +1,10 @@
 module Main where
 
 import Test.Tasty
--- import Test.Tasty.SmallCheck as SC
--- import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
 
 import Language.Plover.Types
-import Language.Plover.Expressions as Exprs
-import Language.Plover.Compile (testWithGcc)
+import Language.Plover.Main
 
 -- import Simplify
 
@@ -25,3 +22,4 @@ tests :: TestTree
 tests = testGroup "Tests" [
   testGroup "Units" $ map gccCase Exprs.good_cases
   ]
+
