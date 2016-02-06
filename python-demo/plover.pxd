fed1aec4288e71193698fd5ebeae58d1a64ab32e
Scott Kovach <dskovach@gmail.com>
2016-02-05 18:30:19 -0800

    add new kalman predict function and binding

--- python-demo/plover.pxd
+++ python-demo/plover.pxd
@@ -2,3 +2,4 @@ from prelude cimport s32
 
 cdef extern from "main.c":
     double test (const s32 n, double * m, double * x);
+    void kp (const s32 dim, const double * x, const double * P, const double * F, const double * Q, double * x_new, double * P_new);
--- python-demo/plover.pyx
+++ python-demo/plover.pyx
@@ -9,3 +9,15 @@ def wrapper(np.ndarray[double, ndim=1, mode="c"] nparr):
     cdef np.ndarray[double, ndim=2, mode="c"] npmat = np.zeros([n, n])
     out = plover.test(n, &npmat[0,0], &nparr[0])
     return (out, npmat, nparr)
+
+def kalman_predict(
+        np.ndarray[double, ndim=1, mode="c"] x,
+        np.ndarray[double, ndim=2, mode="c"] P,
+        np.ndarray[double, ndim=2, mode="c"] F,
+        np.ndarray[double, ndim=2, mode="c"] Q,
+        ):
+    dim = len(x)
+    cdef np.ndarray[double, ndim=1, mode="c"] x_new = np.zeros([dim])
+    cdef np.ndarray[double, ndim=2, mode="c"] P_new = np.zeros([dim, dim])
+    plover.kp(dim, &x[0], &P[0,0], &F[0,0], &Q[0,0], &x_new[0], &P_new[0,0])
+    return (x_new, P_new)
