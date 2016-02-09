1af34a542c89f392889f35def12845f4345df333
Scott Kovach <dskovach@gmail.com>
2016-02-09 14:16:43 -0800

    remove junk

--- python-demo/plover.pyx
+++ python-demo/plover.pyx
@@ -3,13 +3,6 @@ import numpy as np
 cimport numpy as np
 from libc.stdlib cimport malloc, free
 
-# example wrapper
-def wrapper(np.ndarray[double, ndim=1, mode="c"] nparr):
-    n = len(nparr)
-    cdef np.ndarray[double, ndim=2, mode="c"] npmat = np.zeros([n, n])
-    out = plover.test(n, &npmat[0,0], &nparr[0])
-    return (out, npmat, nparr)
-
 def kalman_predict(
         np.ndarray[double, ndim=1, mode="c"] x,
         np.ndarray[double, ndim=2, mode="c"] P,
@@ -39,13 +32,3 @@ def inverse(np.ndarray[double, ndim=2, mode="c"] P):
     cdef np.ndarray[double, ndim=2, mode="c"] new = np.zeros([dim, dim])
     plover.matrix_inverse(dim, &P[0,0], &new[0,0])
     return new
-
-def print_all(
-        np.ndarray[double, ndim=1, mode="c"] innovation,
-        np.ndarray[double, ndim=2, mode="c"] S,
-        np.ndarray[double, ndim=2, mode="c"] Si,
-        np.ndarray[double, ndim=2, mode="c"] K,
-        np.ndarray[double, ndim=1, mode="c"] x_new,
-        np.ndarray[double, ndim=2, mode="c"] P_new):
-    dim = len(innovation)
-    plover.print_all_(dim, &innovation[0], &S[0,0], &Si[0,0], &K[0,0], &x_new[0], &P_new[0,0])
